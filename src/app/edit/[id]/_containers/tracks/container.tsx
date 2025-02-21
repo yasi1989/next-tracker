51c122857733012a68493e88af04f5624ddbc262
yasi1989 <kei.kobayashi.works1989@gmail.com>
2025-02-21 14:36:37 +0900

    fix:デプロイエラー解消(dynamicIOを無効)

--- src/app/edit/[id]/_containers/tracks/container.tsx
+++ src/app/edit/[id]/_containers/tracks/container.tsx
@@ -1,11 +1,8 @@
 import TrackEditForm from "@/app/_features/tracks/form/TrackEditForm";
 import { getTrack } from "@/app/_lib/actions";
 
-type EditTrackContainerProps = {
-  id: string;
-};
-
-const EditTrackContainer = async ({ id }: EditTrackContainerProps) => {
+const EditTrackContainer = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
   const track = await getTrack(id);
   return <TrackEditForm track={track} />;
 };
--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -2,15 +2,10 @@ import { Suspense } from "react";
 import Skelton from "@/app/_features/tracks/form/form-skelton";
 import EditTrackContainer from "./_containers/tracks/container";
 
-type EditTrackParams = {
-  params: Promise<{ id: string }>;
-};
-
-const EditTrack = async ({ params }: EditTrackParams) => {
-  const { id } = await params;
+const EditTrack = async ({ params }: { params: Promise<{ id: string }> }) => {
   return (
     <Suspense fallback={<Skelton />}>
-      <EditTrackContainer id={id} />
+      <EditTrackContainer params={params} />
     </Suspense>
   );
 };
